9b3dabd07bbc3cf42ddb6b51523b27c36157c10f
maxhuldin <maxhuldin@gmail.com>
2016-04-21 18:23:34 +0200

    Pause
    början på ett pause GUI

--- Assets/scripts/pause.cs
+++ Assets/scripts/pause.cs
@@ -4,10 +4,20 @@ using System.Collections;
 public class pause : MonoBehaviour
 {
     bool canpause;
+    private GameObject raycube;
+    private Animator animate;
+    private GUIContent content;
     void Start()
     {
+
         canpause = true;
+        raycube = GameObject.Find("RayCube");
+        animate = GameObject.Find("walkSpritesheet_0").GetComponent<Animator>();
     }
+
+
+
+
     void Update()
     {
 
@@ -17,18 +27,35 @@ public class pause : MonoBehaviour
             {
                 Time.timeScale = 0;
                 canpause = false;
-               
-               
+                raycube.SetActive(false);
+                animate.enabled = false;
+
             }
             else
             {
                 Time.timeScale = 1;
                 canpause = true;
+                raycube.SetActive(true);
+                animate.enabled = true;
             }
         }
     }
 
+    void OnGUI()
+    {
+        if (!canpause)
+        {
+            
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 250, 50), "Resume"))
+            {
+                Time.timeScale = 1;
+                canpause = true;
+                raycube.SetActive(true);
+                animate.enabled = true;
 
+            }
+        }
+    }
 
 
 

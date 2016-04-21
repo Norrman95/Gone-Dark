e93f5070e5ee2c599c52f9f9b200c68df18756a2
godofnanners <casper.martensson@telia.com>
2016-04-21 13:35:02 +0100

    camera och raycube
    camera följer bättre raycube följer gubben

---
+++ Assets/scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private GameObject mousechecker;
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        mousechecker = GameObject.Find("RayCube");
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerInfo = player.transform.position;
+        mousechecker.transform.position = playerInfo;
+	
+	}
+}

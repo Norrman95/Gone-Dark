9a3a4f2eabf9d4ff8fea7f238f908f162580b690
godofnanners <casper.martensson@telia.com>
2016-05-24 14:15:13 +0200

    Revert "alarm enemy och la till backpack texture"
    This reverts commit bc33a1f3a6e3b6a6b82d27b96be80835c867cb7b.

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -10,7 +10,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         public NavMeshAgent agent { get; private set; }          
         private GameObject player;
         public static float Sight_Width, Sight_Range;
-        public bool Alarm;
+
 
         private void Start()
         {
@@ -23,7 +23,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             Sight_Range = 5;
             Sight_Width = 90;
 
-            Alarm = false;
             GetComponent<AudioSource>().Play(22000);
         }
 
@@ -40,7 +39,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             }
         }
 
-        public bool CanSeePlayer()
+        bool CanSeePlayer()
         {
             Vector3 startVec = transform.position;
             Vector3 startVecFwd = transform.forward;
@@ -62,12 +61,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                     else
                         return false;
                 }
-            if (Alarm == true)
-            {
-                return true;
-            }
             return false;
-        }
-             
+        }     
     }
 }
--- Assets/scripts/EnemyScripts/Alarm.cs
+++ Assets/scripts/EnemyScripts/Alarm.cs
@@ -1,37 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityStandardAssets.Characters.ThirdPerson;
 
-public class Alarm : MonoBehaviour
+public class Alarm : MonoBehaviour 
 {
-    public GameObject[] enemy;
-    private AICharacterControl aicharactercontrol;
-
-
-    void Start()
+    
+	void OnTriggerEnter(Collider other)
     {
-        enemy = GameObject.FindGameObjectsWithTag("Enemy");
-
-
-
-    }
-    void OnTriggerEnter(Collider other)
-    {
-
         if (other.gameObject.tag == "Player")
         {
-            for (int i = 0; i < enemy.Length; i++)
-            {
-                if (Vector3.Distance(transform.position, enemy[i].transform.position) < 10)
-                {
-
-                    enemy[i].GetComponent<AICharacterControl>().Alarm=true;
-                    //enemy[i].GetComponent<AICharacterControl>().agent.enabled = true;
-                    Debug.Log("collide and enemy nearby");
-
-                }
-
-            }
+            
         }
     }
 }

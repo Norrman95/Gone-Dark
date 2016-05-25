16647a74106632d8cdd0c6b19cb1a326ee27a27c
maxhuldin <maxhuldin@gmail.com>
2016-05-26 01:25:27 +0200

    the end

---
+++ Assets/TheEnd.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheEnd : MonoBehaviour
+{
+    bool showGUI = false;
+    bool end = false;
+    GUIContent content;
+    public Texture texture;
+    GameObject player;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        content = new GUIContent("", texture, "");
+    }
+
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            showGUI = true;
+
+        }
+    }
+
+    void OnGUI()
+    {
+        if (showGUI)
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), content);
+            Time.timeScale = 0;
+        }
+
+
+    }
+
+
+
+
+
+
+    void Update()
+    {
+
+    }
+
+
+
+}

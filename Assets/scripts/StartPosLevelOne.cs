af7673da68aa68cdd44ab36296885fb8394f8507
maxhuldin <maxhuldin@gmail.com>
2016-05-26 00:17:18 +0200

    ljud

--- Assets/scripts/StartPosLevelOne.cs
+++ Assets/scripts/StartPosLevelOne.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class StartPosLevelOne : MonoBehaviour {
 
     Vector3 startPos;
+    
 	
 	void Start () {
-
+        
         startPos = new Vector3(550, -10, -3.1f);
         GameObject.Find("Player").transform.position = startPos;
+       
 
     }
 	
